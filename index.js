a214f8c05cf367aa9fcf75cee2cd45568bba37ed
Alberto Enriquez <136874006+EnriquezAlberto@users.noreply.github.com>
2023-07-28 19:50:26 -0300

    api rest

--- index.js
+++ index.js
@@ -20,3 +20,36 @@ let productos = [
 
 //Rutas
 
+app.get('/api/productos', (req, res) => {
+    res.json(productos);
+});
+
+app.post('/api/productos', (req, res) => {
+    const { id, nombre , precio} = req.body;
+    const nuevoProducto = { id, nombre, precio};
+    productos.push(nuevoProducto);
+    res.status(201).json(nuevoProducto)
+});
+
+app.put('api/productos/:id', (req, res)=> {
+    const id = parseInt(req.params.id)
+    const {nombre, precio} = req.body;
+    const productosIndex = productos.findIndex((product) => product.id  === id);
+    if (productosIndex !== -1) {
+        productos[productosIndex] = { ...productos[productosIndex], nombre, precio };
+        res.json(productos[productosIndex]);
+      } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+      }
+    });
+
+    app.delete('/api/productos/:id', (req, res)=> {
+        const id = parseInt(req.params.id);
+        productos = productos.filter((product) => product.id !==id);
+        res.json({mensaje: 'Producto eliminado correctamente'})
+    });
+
+    app.listen(port, () => {
+        console.log(`Servidor en funcionamiento en http://localhost:${port}`);
+    })
+   
